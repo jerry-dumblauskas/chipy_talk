45630b5f7b381bbc54b854bac7dff45f943fe662
jerry dumblauskas <jdumblauskas@gmail.com>
2015-05-12 20:42:45 -0500

    delete summary command

--- demo.R
+++ demo.R
@@ -2,11 +2,10 @@
 require (gdata)
 
 #con<-download.file("http://mathforum.org/workshops/sum96/data.collections/datalibrary/colleges.XL.zip.xls", "~/Downloads/tst.xls",method="curl")
-xxx<-read.xls("~/Downloads/tst.xls", skip=1)
+dta<-read.xls("~/Downloads/tst.xls", skip=1)
 
 # Multiple Linear Regression Example 
-fit <- lm(Graduation.rate ~ ACT , data=xxx)
-#summary(fit) # show results
+fit <- lm(Graduation.rate ~ ACT , data=dta)
 
-plot(xxx$ACT, xxx$Graduation.rate)
+plot(dta$ACT, dta$Graduation.rate)
 abline(fit)
