5b98ab42a1661b95e6d42d1b541ad38b777805cd
jerry dumblauskas <jdumblauskas@gmail.com>
2015-05-13 17:19:03 -0500

    sync comments

--- demo.R
+++ demo.R
@@ -1,11 +1,15 @@
 #install.packages('gdata')
 require (gdata)
 
+# 1 -- GET THE FILE AND SAVE IT
 #con<-download.file("http://mathforum.org/workshops/sum96/data.collections/datalibrary/colleges.XL.zip.xls", "~/Downloads/tst.xls",method="curl")
+
+# 2 -- READ THE EXCEL FILE AND CREATE A DATAFRAME
 dta<-read.xls("~/Downloads/tst.xls", skip=1)
 
-# Multiple Linear Regression Example 
+# 3 -- RUN THE REGRESSION AND FIT THE MODEL
 fit <- lm(Graduation.rate ~ ACT , data=dta)
 
+# 4 -- DISPLAY DATA
 plot(dta$ACT, dta$Graduation.rate)
 abline(fit)
--- demo1.py
+++ demo1.py
@@ -3,24 +3,23 @@ import statsmodels.api as sm
 import patsy
 import pandas as pd
 import matplotlib.pyplot as plt
-#import requests
+import requests
 
-# GET THE FILE AND SAVE IT
-#r = requests.get("http://mathforum.org/workshops/sum96/data.collections/datalibrary/colleges.XL.zip.xls")
-
+# 1 -- GET THE FILE AND SAVE IT
+# r = requests.get("http://mathforum.org/workshops/sum96/data.collections/datalibrary/colleges.XL.zip.xls")
 # with open("/Users/jerrydumblauskas/Downloads/tst_python.xls", 'wb') as fd:
 #     fd.write(r.content)
 
-# READ THE EXCEL FILE AND CREATE A DATAFRAME
+# 2 -- READ THE EXCEL FILE AND CREATE A DATAFRAME
 df = pd.read_excel("/Users/jerrydumblauskas/Downloads/tst_python.xls", skiprows=1)
 
-# WTF
+# 2.5 -- WTF
 df.rename(columns={'Graduation rate':'Graduation_rate'}, inplace=True)
 
-# RUN THE REGRESSION AND FIT THE MODEL
+# 3 -- RUN THE REGRESSION AND FIT THE MODEL
 y, X = patsy.dmatrices("Graduation_rate ~ ACT", data=df, return_type='dataframe')
 result_model = sm.OLS(y, X) .fit()   # Describe and fit model
 
-# DISPLAY THE GRAPH
+# 4 -- DISPLAY DATA
 fig = sm.graphics.plot_fit(result_model, "ACT")
 plt.show()
